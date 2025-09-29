c12e4246ba39d859d09c6a9dc819f2c45e94e7e9
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-09-29 16:21:48 +0200

    Created the auth-routes for signup, signin and signout

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from 'express'
+import { PORT } from './config/env.js'
+import authRouter from './routes/auth.routes.js'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1/auth', authRouter)
 
 app.get('/', (req, res) => {
   res.send('hello')
 })
 
-app.listen(4400, () => {
-  console.log('Is running on http://localhost4400')
+
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`)
 })
